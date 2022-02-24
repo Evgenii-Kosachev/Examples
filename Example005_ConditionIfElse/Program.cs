797643c6053ed5453f0ea647615b4538f62d8f0d
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-02-24 18:55:55 +0500

    Example 005 Condition IF-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha"){
+    Console.WriteLine("Hello, Mashaaaa!");
+}else{
+    Console.WriteLine("Good morning " + username);
+}
+
+// ToLower() - Все символы строки переводит в нижний регистр (даже заглавные буквы).
