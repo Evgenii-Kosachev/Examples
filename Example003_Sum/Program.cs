32cd9eab12198044f045ab8cd80738e434371d57
EvgeniiKosachev <evgenii_kosachev@mail.ru>
2022-02-24 18:32:05 +0500

    Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,7 +1,19 @@
-﻿int numberA = 3;
+﻿/*
+int numberA = 3;
 int numberB = 5;
 
 // Console.WriteLine(numberA + numberB);
 
 int result = numberA + numberB;
 Console.Write(result);
+*/
+
+// Пример с генератором случайных чисел.
+int numberA = new Random().Next(1, 10);  // 1234...9
+Console.WriteLine(numberA);
+
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+
+int result = numberA + numberB;
+Console.WriteLine(result);
